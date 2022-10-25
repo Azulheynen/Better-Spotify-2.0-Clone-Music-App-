83dfebc9721ab547098de28e71fe17ae3cb4d5c9
Azul Heynen <azulheynen@gmail.com>
2022-10-25 18:01:10 -0300

    final commit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,24 +6,29 @@ export const shazamCoreApi = createApi({
         baseUrl: "https://shazam-core.p.rapidapi.com/v1",
         prepareHeaders: (headers) => {
             headers.set("X-RapidAPI-Key", "423cddd674msh276ed776ce013c5p1fda04jsn63f66039c2f2");
+
             return headers;
         },
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => "/charts/world" }),
-        getSongDetails: builder.query({
-            query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({
+            query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
         }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-        getArtistDetails: builder.query({
-            query: ({ artistId }) => {
-                console.log("HERE", artistId);
-
-                return `/artists/details?artist_id=${artistId}`;
-            },
-        }),
     }),
 });
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery } =
-    shazamCoreApi;
+export const {
+    useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+} = shazamCoreApi;
